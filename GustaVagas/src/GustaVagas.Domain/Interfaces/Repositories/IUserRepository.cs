507ee6d3b40cf68d0b0ec4dbdfeddf296f580576
Leandro Romanelli <romanelli_leandro@hotmail.com>
2021-01-30 20:40:31 -0300

    Criacao do projeto

---
+++ GustaVagas/src/GustaVagas.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using GustaVagas.Domain.Entities;
+using GustaVagas.Domain.Interfaces.Repositories.Base;
+
+namespace GustaVagas.Domain.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
---
+++ GustaVagas/src/GustaVagas.Infra/Repositories/Base/Repository.cs
@@ -0,0 +1,34 @@
+﻿using GustaVagas.Domain.Interfaces.Repositories.Base;
+using System;
+using System.Collections.Generic;
+
+namespace GustaVagas.Infra.Repositories.Base
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
