070d8568e35b586dcc870e5925b66c6da2c249cc
Gustavo <Gustavo@LAPTOP-QPH319K9>
2021-02-01 17:58:35 -0300

    Segunda Subida
    Mais Duas tabelas concluídas, o que significa que o nosso trabalho está fluíndo, agora temos um modelo de arquitetura decente, coisa essa que creio ser importante no desenvolvimento desse software.

---
+++ GustaVagas/src/GustaVagas.Domain/Entities/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GustaVagas.Domain.Entities.Base;
+
+namespace GustaVagas.Domain.Entities
+{
+    class Area : Entity
+    {
+        public Area() : base()
+        {
+
+        }
+        [StringLength(25)]
+        public string NameArea { get; set; }
+    }
+}
