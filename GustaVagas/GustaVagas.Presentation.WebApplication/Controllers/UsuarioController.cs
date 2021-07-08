29aa055d3e45ca2ebe65d3a70648f4d54d28f2e1
Gustavo6401 <gsoey2019123@gmail.com>
2021-07-08 20:35:59 -0300

    Página de Login e de criação de usuário foi criada com sucesso.

---
+++ GustaVagas/GustaVagas.Presentation.WebApplication/Controllers/UsuarioController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GustaVagas.Infra.Repositories;
+using GustaVagas.Domain.Entities;
+
+namespace GustaVagas.Presentation.WebApplication.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            UsuarioRepository repository = new();
+            return View(repository.Get());
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,UserName,EMail,Password")] Usuario user)
+        {
+            try
+            {
+                UsuarioRepository repository = new();
+                repository.Add(user);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind("Id,UserName,EMail,Password")] Usuario user)
+        {
+            UsuarioRepository repository = new();
+            Usuario usuario = repository.FazerLogin(user.EMail, user.Password);
+
+            while(usuario == null)
+            {                
+                usuario = repository.FazerLogin(user.EMail, user.Password);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
